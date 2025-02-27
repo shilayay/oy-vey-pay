93446f8e6ea400caea33314044748479a0d12b5a
shilaylay <shiloplayz@gmail.com>
2025-02-27 10:21:08 +0200

    new

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://oy-vey-pay.onrender/api': "http://localhost:3000/api"
 export default axios.create({
-    baseURL:'http://localhost:3000/api',
+    baseURL,
     headers:{
         'Content-type':'application/json',
     },
